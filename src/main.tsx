9c341685fe76dcebad842f9ff3ac1e3f57b1d29c
frontend-en <kristinarwebdev@gmail.com>
2024-08-03 18:35:58 +0300

    workflow

--- src/main.tsx
+++ src/main.tsx
@@ -6,7 +6,7 @@ import { TonConnectUIProvider } from "@tonconnect/ui-react";
 
 // this manifest is used temporarily for development purposes
 const manifestUrl =
-  "https://DeLaVerrierr.github.io/first_contract_front_end/tonconnect-manifest.json";
+  "https://frontend-en.github.io/first_contract_front_end/tonconnect-manifest.json";
 
 ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
   <TonConnectUIProvider manifestUrl={manifestUrl}>
